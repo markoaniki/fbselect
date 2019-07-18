b53fceff996d477f261bec4e7c9be440e9030afa
Marko Aniki <markocme2@gmail.com>
2019-07-18 16:46:58 -0300

    Plataforma: Ajustes nas configurações de Player; Math Invaders: Aplicação da animação de ENTER, Adição da função ENTER para iniciar o jogo.

---
+++ FB SELEÇÃO/Assets/Games/Math Invaders/Script/BotaoEntrar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BotaoEntrar : MonoBehaviour
+{
+    public string toCall;
+    // public AudioClip audClip;
+    // public AudioSource audSauce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // audSauce.clip = audClip;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            SceneManager.LoadScene(toCall);
+        }
+    }
+}
