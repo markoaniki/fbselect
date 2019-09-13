519a0fa65e7b0effc3534814879808ac96a16efb
Marko Aniki <markocme2@gmail.com>
2019-09-13 13:20:40 -0300

    Coisas, menu PT...

---
+++ FB SELEÇÃO/Assets/UCScene/IrParaScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IrParaScene : MonoBehaviour
+{
+    public string retPath = "";
+    public string MapPath = "";
+
+    public void IrPara()
+    {
+        Debug.Log("OK");
+
+        SceneManager.LoadScene(retPath, LoadSceneMode.Single);
+    }
+}
